ade34697d05ff4869eaa2721d244263d9aedf40a
RobertGYoung <rgyoungideas@gmail.com>
2020-03-11 13:52:43 -0400

    first commit to Jenkins Project

---
+++ Maven02JenkinsJob/src/main/java/MathOperations.java
@@ -0,0 +1,17 @@
+
+public class MathOperations {
+	public static int add(int i, int j) {
+		return i+j;
+	}
+	public static int sub(int i, int j) {
+		return i-j;
+	}
+	public static int div(int i, int j) {
+		return i/j;
+	}
+	public static int mult(int i, int j) {
+		return i*j;
+	}
+	
+	
+}
